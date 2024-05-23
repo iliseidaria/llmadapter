ac12e08fb9c00060b71e30fcdde47133ce81646b
Koroem <conduratvictor@yahoo.com>
2024-05-23 15:25:28 +0300

    Refactor and fixes

--- controllers/Image.js
+++ controllers/Image.js
@@ -1,9 +1,52 @@
-async function generateImage(){}
-async function editImage(){}
-async function generateImageVariants(){}
+import * as Request from '../utils/request.js';
+import * as Image from '../handlers/Image.js';
+
+async function generateImageEdit(request, response) {
+    const { modelName, prompt, apiKey } = req.body;
+    try {
+
+        Request.sendResponse(response, 200, "application/json", {
+            success: true,
+            data: modelResponse
+        });
+    } catch (error) {
+        Request.sendResponse(response, error.statusCode || 500, "application/json", {
+            success: false,
+            message: error.message
+        });
+    }
+}
+async function generateImage(request, response) {
+    const { modelName, prompt, apiKey } = req.body;
+    try {
+        const modelResponse = await Image.generateImage(apiKey, modelName, prompt);
+        Request.sendResponse(response, 200, "application/json", {
+            success: true,
+            data: modelResponse
+        });
+    } catch (error) {
+        Request.sendResponse(response, error.statusCode || 500, "application/json", {
+            success: false,
+            message: error.message
+        });
+    }
+}
+async function generateImageVariants(request, response) {
+    try {
+        Request.sendResponse(response, 200, "application/json", {
+            success: true,
+            data: modelResponse
+        });
+    } catch (error) {
+        Request.sendResponse(response, error.statusCode || 500, "application/json", {
+            success: false,
+            message: error.message
+        });
+    }
+}
 
 export {
+    generateImageEdit,
     generateImage,
-    editImage,
     generateImageVariants
 }
