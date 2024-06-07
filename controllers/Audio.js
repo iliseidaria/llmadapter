a5061f0db75b300b91cca389c9afe7e59087d4c6
alupoaiemircea <mircea@axiologic.net>
2024-06-07 16:31:58 +0300

    fix audio list voices

--- controllers/Audio.js
+++ controllers/Audio.js
@@ -25,7 +25,6 @@ async function textToSpeech(request, response){
     const result = await fetch(url, options);
     result.body.pipe(response);
 }
-let ttsConfigs;
 async function listVoicesAndEmotions(request, response){
     const url = 'https://api.play.ht/api/v2/voices';
     const options = {
@@ -37,36 +36,27 @@ async function listVoicesAndEmotions(request, response){
         }
     };
     try {
-        if(ttsConfigs){
-            Request.sendResponse(response, 200, 'application/json', {
-                success: true,
-                data: ttsConfigs
-            });
-        } else {
-            const result = await fetch(url, options);
-            const voices = await result.json();
-            let configs = {
-                voices: voices,
-                emotions: ['female_happy' ,
-                    'female_sad' ,
-                    'female_angry' ,
-                    'female_fearful' ,
-                    'female_disgust' ,
-                    'female_surprised' ,
-                    'male_happy' ,
-                    'male_sad' ,
-                    'male_angry' ,
-                    'male_fearful' ,
-                    'male_disgust' ,
-                    'male_surprised']
-            }
-            ttsConfigs = JSON.parse(JSON.stringify(configs));
-            Request.sendResponse(response, 200, 'application/json', {
-                success: true,
-                data: configs
-            });
+        const result = await fetch(url, options);
+        const voices = await result.json();
+        let configs = {
+            voices: voices,
+            emotions: ['female_happy' ,
+                'female_sad' ,
+                'female_angry' ,
+                'female_fearful' ,
+                'female_disgust' ,
+                'female_surprised' ,
+                'male_happy' ,
+                'male_sad' ,
+                'male_angry' ,
+                'male_fearful' ,
+                'male_disgust' ,
+                'male_surprised']
         }
-
+        Request.sendResponse(response, 200, 'application/json', {
+            success: true,
+            data: configs
+        });
     } catch (e) {
             Request.sendResponse(response, 500, 'application/json', {
             success: false,
