556364ca25f4268bc2ecb835439372bf4d308bdf
skutner <sava.dumitru.daniel@gmail.com>
2025-01-30 09:51:54 +0200

    Add request logging middleware to server
    This introduces a `requestLogger` middleware that logs HTTP request details, including method, URL, status code, and response time. The middleware enhances observability and helps in monitoring server activity.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import fsPromises from 'fs/promises';
 const serverConfig =  await fsPromises.readFile('./config.json', 'utf-8').then(JSON.parse);
 
 import bodyReader from './middlewares/bodyReader.js';
+import requestLogger from "./middlewares/requestLogger.js";
 
 class Server {
     constructor() {
@@ -49,6 +50,7 @@ class Server {
 const server = new Server();
 
 server.use(bodyReader);
+server.use(requestLogger);
 
 server.listen(serverConfig.PORT, () => {
     console.log(`Server running on http://localhost:${serverConfig.PORT}/`);
