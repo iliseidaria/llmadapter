b39358ea3b2dd264991d758bbce080b3a2c0a391
iliseidaria <iliseidaria@yahoo.com>
2025-06-04 23:15:54 +0300

    delete merged code from refactoring

--- handlers/utils/streamEmitter.js
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from 'events';
-
-class LLMStreamingEmitter extends EventEmitter {}
-
-const createStreamEmitter = () => {
-    return new LLMStreamingEmitter();
-};
-
-export { createStreamEmitter };
