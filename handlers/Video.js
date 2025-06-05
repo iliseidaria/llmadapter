0902f18e87139b89e595cc91e918545b59f11059
iliseidaria <iliseidaria@yahoo.com>
2025-06-05 04:27:22 +0300

    removed Windows-specific files

--- handlers/Video.js
+++
@@ -1,9 +0,0 @@
-import LLMFactory from './factory/LLMFactory.js';
-
-async function lipsync(APIKey, modelName, configs) {
-    const modelInstance = await LLMFactory.createLLM(modelName, APIKey, configs);
-    return await modelInstance.lipsync();
-}
-export {
-    lipsync
-}
